951cdb975a5483521d1750dbd982821b99ff67c3
root <root@kryloff.>
2025-09-07 22:56:26 +0300

    Some checks fixes

--- krylov.matvey/S4/main.cpp
+++ krylov.matvey/S4/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char** argv)
     {
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-      std::cout << "<INVALID COMMAND>\n";
+      std::cout << e.what() <<"\n";
     }
 
   }
