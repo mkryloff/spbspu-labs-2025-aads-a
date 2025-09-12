14fbca08614e67bf68e3abf815ac6385d04557a5
root <root@kryloff.>
2025-09-12 22:08:40 +0300

    Main probably done, need to check

--- krylov.matvey/S5/main.cpp
+++ krylov.matvey/S5/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <functional>
 #include <binaryTree.hpp>
+#include "keySummer.hpp"
+
+namespace
+{
+  using biTree = krylov::BiTree< int, std::string, std::less< int > >;
+  void callAscending(const biTree & tree, krylov::KeySummator& result)
+  {
+    result = tree.traverseLnr(result);
+  }
+
+  void callDescending(const biTree & tree, krylov::KeySummator& result)
+  {
+    result = tree.traverseRnl(result);
+  }
+
+  void callBreadth(const biTree& tree, krylov::KeySummator& result)
+  {
+    result = tree.traverseBreadth(result);
+  }
+}
 
 int main(int argc, char** argv)
 {
@@ -25,6 +46,27 @@ int main(int argc, char** argv)
   if (!file.eof())
   {
     std::cerr << "Incorrect input!\n";
+    return 1;
+  }
+  if (biTree.empty())
+  {
+    std::cout << "<EMPTY>\n";
+    return 0;
+  }
+  BiTree< std::string, std::function< void(KeySummator&) >, std::less< std::string > > commands;
+  using namespace std::placeholders;
+  commands["ascending"] = std::bind(callAscending, std::cref(biTree), _1);
+  commands["descending"] = std::bind(callDescending, std::cref(biTree), _1);
+  commands["breadth"] = std::bind(callBreadth, std::cref(biTree), _1);
+  try
+  {
+    KeySummator result;
+    commands.at(argv[1])(result);
+    std::cout << result.keySum << " " << result.values << "\n";
+  }
+  catch (const std::out_of_range & e)
+  {
+    std::cerr << "<INVALID COMMAND>\n";
     return 1;
   }
 }
