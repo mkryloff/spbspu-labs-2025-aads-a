dbef2a28103872ca1d08a1111c2511f535a4e6dd
root <root@kryloff.>
2025-09-12 20:17:04 +0300

    File read implemented

--- krylov.matvey/S5/main.cpp
+++ krylov.matvey/S5/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
+#include <binaryTree.hpp>
 
 int main(int argc, char** argv)
 {
+  using namespace krylov;
   if (argc != 3)
   {
     std::cerr << "Wrong arguments\n";
@@ -12,5 +14,17 @@ int main(int argc, char** argv)
   if (!file)
   {
     std::cerr << "Wrong filename\n";
+  }
+  std::string value;
+  int key = 0;
+  BiTree< int, std::string, std::less< int > > biTree;
+  while ((file >> key >> value) && !file.eof())
+  {
+    biTree[key] = value;
+  }
+  if (!file.eof())
+  {
+    std::cerr << "Incorrect input!\n";
+    return 1;
   }
 }
