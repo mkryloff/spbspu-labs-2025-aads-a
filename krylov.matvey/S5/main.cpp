7d4b47b619538cdf2c883830d6396db6778bbc2e
root <root@kryloff.>
2025-09-12 20:05:17 +0300

    Input checks done

--- krylov.matvey/S5/main.cpp
+++ krylov.matvey/S5/main.cpp
@@ -1,2 +1,16 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv)
+{
+  if (argc != 3)
+  {
+    std::cerr << "Wrong arguments\n";
+    return 1;
+  }
+  std::ifstream file(argv[2]);
+  if (!file)
+  {
+    std::cerr << "Wrong filename\n";
+  }
+}
